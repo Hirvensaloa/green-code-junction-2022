a30c5ad3f986ad72be8f47fe98c29691a3936e0c
sirlarion <miska.tammenpaa@gmail.com>
2022-11-05 22:19:28 +0200

    improve text content view

--- client/views/TextPost/index.tsx
+++ client/views/TextPost/index.tsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 import { Button } from "../../components/Button";
 import { theme } from "../../styles/theme";
 import { EnergyIcon } from "../../components/EnergyIcon";
-import { MultilineInput } from "../../components/Input";
-import { Heading3, Text } from "../../styles/typography";
+import { MultilineInput, Input } from "../../components/Input";
+import { Heading3, headingBase, Text } from "../../styles/typography";
 import { useActiveContent } from "../../hooks/useActiveContent";
 import { useEnergy } from "../../hooks/useEnergy";
 
@@ -32,9 +32,28 @@ const Container = styled.section`
   }
 `;
 
-const StyledInput = styled(MultilineInput)`
+const InputTitle = styled(Input)`
+  ${headingBase};
+  font-size: 1.5rem;
+  width: 100%;
+  padding: 0.5rem 0;
+  ::placeholder {
+    ${headingBase}
+    opacity: 0.7;
+  }
+`;
+
+const Line = styled.div`
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: ${theme.background.primary};
+`;
+
+const InputContent = styled(MultilineInput)`
   width: 100%;
   height: 40vh;
+  padding: 1.5rem 0;
 `;
 
 const SubmitButton = styled(Button)`
@@ -47,7 +66,9 @@ const SubmitButton = styled(Button)`
   background-color: ${theme.accent.electricBlue};
 `;
 
-const ButtonText = styled(Heading3)``;
+const ButtonText = styled(Heading3)`
+  font-family: "Lilita One";
+`;
 
 export const TextPost = () => {
   const { decreaseEnergy } = useEnergy();
@@ -69,7 +90,9 @@ export const TextPost = () => {
       </InfoCard>
       <Card>
         <form onSubmit={handleSubmit}>
-          <StyledInput type="text" placeholder="What's on your mind?" />
+          <InputTitle type="text" placeholder="Title for the post" />
+          <Line />
+          <InputContent type="text" placeholder="What's on your mind?" />
           <SubmitButton type="submit">
             <ButtonText $dark={true}>Post</ButtonText>
             <EnergyIcon type="black" />
